ede614a1e9832cd451f73f67ae121ba9f1e33feb
nadinealysse <85436997+nadinealysse@users.noreply.github.com>
2024-01-05 13:52:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function sayHi() {
     let greetings = ["Hi", "Hello", "Hey", "Sup", "Wassup"];
     let greeting = greetings[Math.floor(greetings.length * Math.random())];
     console.log(greeting);
+    console.log("hi");
     }
