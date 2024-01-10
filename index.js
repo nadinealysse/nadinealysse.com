64f7c35813b12074992ad965e96b11a81a17d711
nadinealysse <85436997+nadinealysse@users.noreply.github.com>
2024-01-10 18:57:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function sayHi() {
     let greeting = greetings[Math.floor(greetings.length * Math.random())];
     console.log(greeting);
     console.log("hi");
-    console.log("i'm trying to get my ITF+ and the trifecta");
+    console.log("i'm trying to get my ITF+, Linux+, PenTest+ and the Trifecta!");
     }
